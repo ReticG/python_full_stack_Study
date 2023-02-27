113d604c89ad83d199471cc9f42334613559e3fc
GJC <332982539@qq.com>
2023-02-27 10:08:17 +0800

    day 01 first -2

--- CodeExercise/first.py
+++ CodeExercise/first.py
@@ -1,1 +1,13 @@
 print("Hello World")
+
+# 例如： 找到在当前目录下，所有 png 格式的文件并打印
+import os
+
+for item in os.listdir("D:\Project\PycharmProjects\python_full_stack_Study\CodeExercise"):
+    if item.endswith('png'):
+        print(item)
+
+# 输出不换行
+print("xxxxxx", end="")
+print("yyyyyy")
+
